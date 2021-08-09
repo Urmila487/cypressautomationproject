fae4c86514a97da5f3c5f5ecde827113bfe7cdde
UrmilaVadi <gopi.ghadiya@gmail.com>
2021-08-09 13:33:27 +0300

    New file added

--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -1,16 +1,19 @@
 ///<reference types="Cypress" />
 
 describe("Test contact-us from a WebDriver University",() => {
-    it("Should be able to submit a successful submission via contact us from" , () => {
+    it.only("Should be able to submit a successful submission via contact us from" , () => {
        //Cypress code 
         //cy.visit('http://www.webdriveruniversity.com/index.html')
         //cy.get('#contact-us').click({force:true})
         cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html')
+        cy.document().should('have.property','charset').and('eq','UTF-8')
+        cy.get('h2').should('have.text','CONTACT US')
         cy.get('[name="first_name"]').type('Urmila')
         cy.get('[name="last_name"]').type('vadi')
         cy.get('[name="email"]').type('urmila@gmail.com')
         cy.get('textarea.feedback-input').type('I want to signup this web university')
         cy.get('[type="submit"]').click()
+        cy.get('h1').should('have.text','Thank You for your Message!')
 
     })
 
@@ -21,6 +24,7 @@ describe("Test contact-us from a WebDriver University",() => {
         cy.get('[name="last_name"]').type('vadi')
         cy.get('textarea.feedback-input').type('I want to signup this web university')
         cy.get('[type="submit"]').click()
+        cy.get('body').contains('Error: all fields are required')
  
  
  
--- cypress/integration/webdriver-uni/selectors.js
+++ cypress/integration/webdriver-uni/selectors.js
@@ -1,4 +1,5 @@
 ///<reference types="Cypress" />
+/// <reference types="cypress-xpath" />
 
 describe("Test contact-us from a WebDriver University",() => {
     it("Should be able to submit a successful submission via contact us from" , () => {
@@ -11,6 +12,23 @@ describe("Test contact-us from a WebDriver University",() => {
         //By attribute name and value
 
         cy.get("input[name='first_name']")
+
+        //By id
+        cy.get("#contact_me")
+
+        //By class
+        cy.get(".feedback-input")
+
+        //By multiple classes
+        cy.get("[class='navbar navbar-inverse navbar-fixed-top")
+
+        //By different attributes
+        cy.get("input[name='email'][placeholder='Email Address']")
+
+
+        //By Xpath
+        cy.xpath("//input[@name='first_name']").type('Urmila')
+
 
     })
     })

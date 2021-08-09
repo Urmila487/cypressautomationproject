19b66ba1b8b44cb9f1c4db7b2970f45675b375d2
UrmilaVadi <gopi.ghadiya@gmail.com>
2021-08-09 14:00:34 +0300

    Run all Test Cases commit

--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -1,12 +1,14 @@
 ///<reference types="Cypress" />
 
 describe("Test contact-us from a WebDriver University",() => {
-    it.only("Should be able to submit a successful submission via contact us from" , () => {
+    it("Should be able to submit a successful submission via contact us from" , () => {
        //Cypress code 
         //cy.visit('http://www.webdriveruniversity.com/index.html')
         //cy.get('#contact-us').click({force:true})
         cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html')
         cy.document().should('have.property','charset').and('eq','UTF-8')
+        cy.title().should('include','WebDriver | Contact Us')
+        cy.url().should('include','contactus')
         cy.get('h2').should('have.text','CONTACT US')
         cy.get('[name="first_name"]').type('Urmila')
         cy.get('[name="last_name"]').type('vadi')
